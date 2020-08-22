53b9aa941661ca579e53cc913acb65bb673436c5
VerticalEye <verticaleye18@gmail.com>
2020-08-22 15:13:56 +0200

    blocca il sub-menu dei servizi e sistema lo stile della home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,3 +55,20 @@ function closeForm(){
   document.getElementsByClassName("form")[0].style.display = "none";
 }
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementsByClassName("sub-menu")[0];
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
