50de0f81b53527673a07610e536c84cc2b119200
VerticalEye <verticaleye18@gmail.com>
2020-08-24 16:39:35 +0200

    sistema js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,19 +56,19 @@ function closeForm(){
 }
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyFunction()};
 
 // Get the navbar
-var navbar = document.getElementsByClassName("sub-menu")[0];
+var subMenu = document.getElementsByClassName("sub-menu")[0];
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = subMenu.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    subMenu.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    subMenu.classList.remove("sticky");
   }
 }
