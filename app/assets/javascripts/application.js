974edd070ca1237aeddfbc2ab99e2794520e80dc
VerticalEye <verticaleye18@gmail.com>
2020-09-02 17:14:45 +0200

    sistema sub-menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,24 @@
 
 /* Set the width of the side navigation to 250px */
 
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = $(".tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = $(".tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -8,29 +8,6 @@ function closeNav() {
 }
 
 
-
-function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = $(".tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Remove the background color of all tablinks/buttons
-  tablinks = $(".tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
-}
-
-
 function openForm(){
   document.getElementsByClassName("form")[0].style.display = "block";
 }
@@ -72,10 +49,13 @@ function check_if_in_view() {
     $('#el-logo').append($gif);
 
   };
+
+
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+
+$("#defaultOpen").click();
+
 
 //Lightbox
 $("#lavori img").click(function(){
@@ -99,7 +79,26 @@ $("#lavori img").click(function(){
     $overlay.hide();
   });
 
- 
+ $window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+
+// When the user scrolls the page, execute myFunction
+$window.on('scroll', stickyFunction);
+// Get the navbar
+var $subMenu = $(".sub-menu");
+
+// Get the offset position of the navbar
+var sticky = $subMenu.offset().top;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    $subMenu.addClass("sticky");
+  } else {
+    $subMenu.removeClass("sticky");
+  }
+};
 
 
   
