23d93f318ecb645beb0bdc858e528947fb65f094
VerticalEye <verticaleye18@gmail.com>
2020-09-22 17:16:26 +0200

    ripristina immagine iniziale

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,5 @@
-$('#initial .logo').append($('<img src="assets/biancovero trasparente_fermoepausa_loop.webp" alt="logo_gif" title="logo_gif" id="prova">'));
+
+
 
 function openNav() {
   $("#mySidenav").css('width', '50%');
